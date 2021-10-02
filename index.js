70b2aa839ce88175c9a7a58565765bb1664b538e
OnidDev <guillaume.rnl@gmail.com>
2021-10-02 14:19:39 +0200

    defv
    dev

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const escapeMd = (str) => str.replace(/([\[\]\\`\(\)])/g, '\\$1')
 
 const { payload: githubPayload } = github.context
 
-const commits = githubPayload.commits.map(i => ` - ${escapeMd(i.message)} - by ${i.author.name}`)
+const commits = githubPayload.commits.map(i => ` - [\`[${shortSha(i.id)}]\`](${i.url}) ${escapeMd(i.message)} - by ${i.author.name}`)
 
 if (!commits.length) {
   return
@@ -30,7 +30,7 @@ const payload = {
   embeds: [
     {
       title: core.getInput('message-title') || 'Commits received',
-      description: `${commits.join('\n')}`
+      description: `[\`\[${shortSha(beforeSha)}...${shortSha(afterSha)}\]\`](${compareUrl})\n${commits.join('\n')}`
     }
   ]
 }
