baf0b68baa17209c3452b2ed46bf1da0aa9b0f2b
PsyloZ <73759706+PsyloZ@users.noreply.github.com>
2024-06-27 22:19:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const github = require('@actions/github')
 const webhook = core.getInput('webhook')
 
 if (!/https:\/\/discord(app|)\.com\/api\/webhooks\/\d+?\/.+/i.exec(webhook)) {
-  core.setFailed('The given discord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"')
+  core.setFailed('The given di scord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"')
 }
 
 const shortSha = (i) => i.substr(0, 7)
